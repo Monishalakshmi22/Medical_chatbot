7637c498a2d52a937ad64b0e7ec5b0a97d3856c7
Monishalakshmi22 <2300030930@klunniversity.in>
2026-06-04 12:52:43 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Medical_chatbot
+
+```bash
+git clone https://github.com/Monishalakshmi22/Medical_chatbot.git
+```
+
+```bash
+conda create -n medibot python = 3.10 -y
+```
+
+'''bash
+conda activate medibot
+```
+
+```bash
+pip install -r requirements.txt
+'''
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Mounisha Maddi',
+    description='A medical chatbot application',
+    packages=find_packages(),
+    author_email= "2300030930@kluniversity.in",
+    install_requires=[]
+)
