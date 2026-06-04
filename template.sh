7229f969d42712c83104e29398ef6b5000540fd4
Monishalakshmi22 <2300030930@klunniversity.in>
2026-06-04 10:47:03 +0530

    folderstructure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+
+#creating directory
+mkdir -p src
+mkdir -p research
+
+#Creating files
+toch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.text
+
+echo "Directory and files created successfully!"
